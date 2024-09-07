f6d1ccc1f3fc7e8b0fff8204643c1bde6ac0ae93
David Vinson <davidvinson018@gmail.com>
2024-09-07 10:59:32 -0500

    http response is working as expected

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,8 @@
 //make onReady function that will load server data when DOM is ready
+
+//global operator to start; can be changed
+let operator = '+';
+
 function onReady() {
   console.log('client.js is sourced!');
   fetchCalculation();
@@ -31,6 +35,35 @@ function renderCalculationHistory(calculations) {
       </ul>
       `;
   }
+}
+
+//form handler
+function submitCalculation(event) {
+  event.preventDefault();
+  //submit form by clicking the equal sign button
+  console.log('submit calculation');
+  let numOneInput = document.getElementById('numOne').value;
+  let numTwoInput = document.getElementById('numTwo').value;
+
+  console.log('Number 1 value:', numOneInput);
+  console.log('Number 2 value', numTwoInput);
+  console.log('Operation', operator);
+
+  //build calculation object and send to server: {num1, num2, operator}
+  const calcObj = {
+    num1: numOneInput,
+    num2: numTwoInput,
+    operator,
+  };
+  axios.post('/calculations', calcObj).then((response) => {
+    console.log('get updates from server...');
+    fetchCalculation();
+  });
+}
+
+function setOperator(event) {
+  console.log('add btn clicked!', event.target);
+  operator = event.target.id;
 }
 
 //call onReady
